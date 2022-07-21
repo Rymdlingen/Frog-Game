e3839782293e6f99a72c4b6489f96a2091adff3e
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2022-07-21 16:39:08 +0200

    Can't make a spring :(

--- Prototype-1/Assets/Scripts/Physics Jump/PhysicsCharacterController.cs
+++ Prototype-1/Assets/Scripts/Physics Jump/PhysicsCharacterController.cs
@@ -18,17 +18,97 @@ public class PhysicsCharacterController : MonoBehaviour
     private bool isJumping;
     private bool isGrounded;
     private Rigidbody playerRb;
+    private CapsuleCollider playerCollider;
     private Vector3 direction;
+
+    [SerializeField] Vector3 currentVelocity;
+
+    private float heightOfPlayerModel;
+    private float goalYPositionOffset;
+
+    [SerializeField] private float rideHeight;
+    [SerializeField] private float rideSpringStrenght;
+    [SerializeField] private float rideSpringDamper;
+
+    float theXfloat;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
+        heightOfPlayerModel = transform.Find("Frog").GetComponent<BoxCollider>().size.y;
+        goalYPositionOffset = (heightOfPlayerModel - transform.GetComponent<CapsuleCollider>().height) / 2;
+        rideHeight = heightOfPlayerModel / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        float deltaTime = Time.deltaTime;
+
+        Physics.Raycast(playerRb.position + playerCollider.center, Vector3.down, out hit, heightOfPlayerModel);
+        Debug.DrawRay(playerRb.position + playerCollider.center, Vector3.down, Color.blue, 1f);
+
+        Vector3 playerVelocity = playerRb.velocity;
+        Vector3 rayDirection = transform.TransformDirection(-transform.up);
+
+        Vector3 otherVelocity = Vector3.zero;
+        Rigidbody hitBody = hit.rigidbody;
+        if (hitBody != null)
+        {
+            otherVelocity = hitBody.velocity;
+        }
+
+        float rayDirectionVelocity = Vector3.Dot(rayDirection, playerVelocity);
+        float otherDirectionVelocity = Vector3.Dot(rayDirection, otherVelocity);
+
+        float relativeVelocity = rayDirectionVelocity - otherDirectionVelocity;
+
+
+        float x = hit.distance - rideHeight;
+        theXfloat = x;
+
+        float springForce = (x * rideSpringStrenght) - (relativeVelocity * rideSpringDamper);
+
+        if (!isJumping)
+        {
+            playerRb.AddForce(rayDirection * springForce);
+        }
+
+        // Spring that keep player on ground
+        if (!isJumping)
+        {
+
+
+
+
+
+            /*
+            float playerYPosition = transform.position.y;
+            float playerYVelocity = playerRb.velocity.y;
+
+            CalculateDampedSpring(ref playerYPosition,
+                ref playerYVelocity,
+                transform.position.y + goalYPositionOffset,
+                deltaTime, 0, 0);
+            */
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        #region Move player
+
         // Move player on ground plane.
         if (isJumping)
         {
@@ -49,10 +129,7 @@ public class PhysicsCharacterController : MonoBehaviour
         direction = horizontalInput * transform.right + verticalInput * transform.forward;
         direction.Normalize();
 
-        /* if (verticalInput == 0)
-         {
-             playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, 0);
-         }*/
+        #endregion Move Player
 
         // Jump
         if (Input.GetKeyDown(KeyCode.Space))
@@ -61,7 +138,6 @@ public class PhysicsCharacterController : MonoBehaviour
             currentGravity = gravityJumpStart;
 
             playerRb.AddForce(transform.up * currentGravity, ForceMode.Impulse);
-
 
             isJumping = true;
             isGrounded = false;
@@ -109,5 +185,23 @@ public class PhysicsCharacterController : MonoBehaviour
     private void MovePlayer()
     {
         playerRb.AddForce(direction * currentSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        ClampMovementSpeed();
+
+        currentVelocity = playerRb.velocity;
+    }
+
+    private void ClampMovementSpeed()
+    {
+
+        playerRb.velocity = new Vector3(Mathf.Clamp(playerRb.velocity.x, -currentSpeed / 10, currentSpeed / 10), playerRb.velocity.y, Mathf.Clamp(playerRb.velocity.z, -currentSpeed / 10, currentSpeed / 10));
+
+    }
+
+    public static void CalculateDampedSpring(ref float position, ref float velocity, float goalPosition, float deltaTime, float frequency, float damping)
+    {
+        if (position != goalPosition)
+        {
+
+        }
     }
 }
