7bcecd9ae78c2dbc7c43f2a2392d574ca5777aa8
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2022-10-03 14:38:06 +0200

    Finished programming for UI.

--- Prototype-1/Assets/Scripts/UI/TutorialUI.cs
+++ Prototype-1/Assets/Scripts/UI/TutorialUI.cs
@@ -6,25 +6,46 @@ public class TutorialUI : MonoBehaviour
 {
     [SerializeField] float startTimer;
     float counter;
+    bool pauseCounter;
+
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private GameObject tutorialUI;
 
     // Start is called before the first frame update
     void Start()
     {
         counter = startTimer;
+        gameManager.pauseGameEvent.AddListener(ShowInstructions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (counter > 0)
+        if (Input.GetKeyDown(KeyCode.I))
         {
+            counter = startTimer;
+        }
+
+        if (pauseCounter)
+        {
+            if (!tutorialUI.activeSelf) tutorialUI.SetActive(true);
+        }
+        else if (counter > 0)
+        {
+            if (!tutorialUI.activeSelf) tutorialUI.SetActive(true);
             counter -= Time.deltaTime;
         }
         else
         {
             // Hide tutorial UI
+            if (tutorialUI.activeSelf) tutorialUI.SetActive(false);
         }
 
         // Listen to if pause is hit. Show if pause and never show in game again TODO
+    }
+
+    private void ShowInstructions(bool gameIsPaused)
+    {
+        pauseCounter = gameIsPaused;
     }
 }
